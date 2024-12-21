01b1ebb2b2eb406a3b75a0226f9c59ab735aa272
Kaguya <kaguya@Kaguya.cn>
2024-12-21 18:34:02 +0800

    all_directory_mode option

--- jsonl_chk.py
+++ jsonl_chk.py
@@ -95,6 +95,7 @@ import copy
 import os
 from pathlib import Path
 from io import BytesIO
+import pickle
 
 parser = argparse.ArgumentParser(description='''Common post-process script for parallel corpus mnbvc. Every corpus file should run this script before datachecker, or the corpus file cannot be accepted then published.
     - convert old-style parallel corpus to new-style parallel corpus
@@ -105,10 +106,11 @@ parser = argparse.ArgumentParser(description='''Common post-process script for p
 ''')
 parser.add_argument('input', type=str, help='The input file path', nargs='?')
 parser.add_argument('-d', '--directory', type=str, help='Process a directory instead of a single file')
+parser.add_argument('-a', '--all_directory_mode', type=str, help='Read all files under given directory, then generate output file using given filename specify by this arg')
 parser.add_argument('-v', '--verbose', action='store_true', help='Print deduplication info')
 parser.add_argument('-dr', '--disable_rename', action='store_true', help='Disable auto assign json `filename` field to its file name')
 parser.add_argument('-dbg', '--debug', action='store_true', help='Print debug info')
-parser.add_argument('-b', '--bytes_limit', type=int, default=536870912, help='Specify the upper limit each output jsonl file in bytes.')
+parser.add_argument('-b', '--bytes_limit', type=int, default=536870912, help='Specify the upper limit each output jsonl file in bytes')
 # parser.add_argument('-ea', '--enable_assert', action='store_true', help='Enable assertions in the script')
 # parser.add_argument('-da', '--disable_auto_dedup', action='store_true', help='Disable auto deduplicate and empty line elimination')
 
@@ -200,7 +202,18 @@ NEW_STYLE_FIELDS = [
     "zh_text_md5",
 ]
 
-def validate_ext_fields(data: dict, first_warn_map: dict, disable_ext_field_check: bool):
+# 文件统计相关的走全局变量
+# 以文件名为主键，不同的文件名不共享行号、行结构、中文去重计数
+first_warn_unk_key = set()
+first_warn_other_texts_key_check = set()
+filename2zh_text_digest = {}
+filename2low_quality_count = Counter()
+filename2linecount = Counter()
+valid_line_idx_set = set()
+filename2zh_text_dedup_count = Counter()
+filename2linedigest = {}
+
+def validate_ext_fields(data: dict, disable_ext_field_check: bool):
     if data.get('扩展字段') is None:
         data['扩展字段'] = data.pop('拓展字段', r'{}')
     if data['扩展字段'] == '':
@@ -215,16 +228,16 @@ def validate_ext_fields(data: dict, first_warn_map: dict, disable_ext_field_chec
             other_texts_field = ext_field.pop('other_texts')
             for k, v in other_texts_field.items():
                 if len(k) != 2 or not k.islower():
-                    if k not in first_warn_map['other_texts_key_check']:
-                        first_warn_map['other_texts_key_check'].add(k)
+                    if k not in first_warn_other_texts_key_check:
+                        first_warn_other_texts_key_check.add(k)
                         print("【警告】other_texts含有key名可能不合ISO 639-1规范的语种双字母缩写，请向工作群报告:", k)
             accepted_fields['other_texts'] = other_texts_field
         if 'k' in ext_field:
             k_field = ext_field.pop('k')
             accepted_fields['k'] = k_field
         for unknown_key, val in ext_field.items():
-            if unknown_key not in first_warn_map['unk_key']:
-                first_warn_map['unk_key'].add(unknown_key)
+            if unknown_key not in first_warn_unk_key:
+                first_warn_unk_key.add(unknown_key)
                 print("【警告】扩展字段含有尚未定义的字段，请向工作群报告:", unknown_key)
             accepted_fields[unknown_key] = val # 打印警告信息，但是允许收录
         ext_field.clear()
@@ -233,7 +246,7 @@ def validate_ext_fields(data: dict, first_warn_map: dict, disable_ext_field_chec
         print("【错误】扩展字段并非有效json字符串：", data['扩展字段'])
         exit(1)
 
-def gen_new_style_line(file_path: Path, first_warn_map: dict, disable_ext_field_check: bool):
+def gen_new_style_line(file_path: Path, disable_ext_field_check: bool):
     with open(file_path, "r", encoding='utf-8') as fi:
         # fic = fi.read() # 直接读40G文件报 Memory Error 了
         # $ wc -l dual_ass.jsonl
@@ -247,7 +260,7 @@ def gen_new_style_line(file_path: Path, first_warn_map: dict, disable_ext_field_
             data: dict = json.loads(linestr)
             if not args.disable_rename:
                 data['文件名'] = file_path.name # 对于游戏语料，这里强制要求文件名等于jsonl内部文件名
-            validate_ext_fields(data, first_warn_map, disable_ext_field_check)
+            validate_ext_fields(data, disable_ext_field_check)
             if '段落' in data: # 旧版语料
                 for pid, p in enumerate(data['段落']):
                     if '时间' not in p or not p['时间']:
@@ -288,16 +301,7 @@ def process_file(file_path: Path):
             os.makedirs(out_file_dir)
         is_first = False
     del out_file_dir
-    first_warn_map = {'unk_key': set(), 'other_texts_key_check': set()}
-    # filename2lines = {}
-    # 以文件名为主键，不同的文件名不共享行号、行结构、中文去重计数
-    filename2linedigest = {}
-    filename2zh_text_digest = {}
-    filename2low_quality_count = Counter()
-    filename2linecount = Counter()
-    valid_line_idx_set = set()
-
-    for lineidx, linejson in enumerate(gen_new_style_line(file_path, first_warn_map, False)):
+    for lineidx, linejson in enumerate(gen_new_style_line(file_path, False)):
         #######去除空行#######
         line_dedup_set = set()
         for lang_field in LANG_FIELDS:
@@ -308,7 +312,6 @@ def process_file(file_path: Path):
             if args.verbose:
                 print('【段落去冗余】为空或不同语种字段全一致的段落:',linejson)
             continue
-        del line_dedup_set
         #######去除空行#######
         linejsonfilename = linejson['文件名']
         #######文件级去重#######，去除所有LANG_FIELDS加上扩展字段，完全一致的段落，如[{"en_text":"Fine","zh_text":"好"},{"en_text":"Fine","zh_text":"好"}],这种重复只保留第一次出现的那段
@@ -317,7 +320,6 @@ def process_file(file_path: Path):
         for lang_field in LANG_FIELDS:
             dedup_dict[lang_field] = linejson[lang_field]
         dedup_bytes = json.dumps(dedup_dict, ensure_ascii=False, sort_keys=True).encode('utf-8')
-        del dedup_dict
         # digest = hashlib.sha256(dedup_str).hexdigest() + hashlib.md5(dedup_str).hexdigest() # 选一个快又不那么容易冲突的办法就行
         # digest = hashlib.sha256(dedup_str).hexdigest()
         digest = hashlib.md5(dedup_bytes).digest() + (len(dedup_bytes) % 256).to_bytes(1, signed=False)
@@ -331,7 +333,7 @@ def process_file(file_path: Path):
         # filelines = filename2lines.setdefault(linejsonfilename, [])
         # filelines.append(lineidx) # 记有效行的下标
         filename2linecount[linejsonfilename] += 1
-        valid_line_idx_set.add(lineidx)
+        valid_line_idx_set.add((str(file_path),lineidx))
 
         #######文件级去重#######
         # 计算【去重段落数】、【低质量段落数】，填写【是否重复】
@@ -346,26 +348,49 @@ def process_file(file_path: Path):
         digest = hashlib.md5(dedup_bytes).digest() + (len(dedup_bytes) % 256).to_bytes(1, signed=False) # 内存瓶颈
         zh_text_set.add(digest)
         # _afterlen = len(zh_text_set)
-    del filename2linedigest
-    filename2zh_text_dedup_count = Counter()
     for filename, zh_text_set in filename2zh_text_digest.items():
         filename2zh_text_dedup_count[filename] = len(zh_text_set)
-    
-    filename2linecounter = Counter()
-    bio = BytesIO()
-    out_file_id = 1
-    next_out_file_path = file_path.parent / "jsonl_reworked" / file_path.name
-    filename_without_ext, file_ext_name = file_path.name.rsplit('.', 1)
 
-    # with open(out_file_path, "w", encoding='utf-8') as fo: # 弄个1G缓存吧
-    for lineidx, linejson in enumerate(gen_new_style_line(file_path, first_warn_map, True)):
-        if lineidx not in valid_line_idx_set:
+filename2linecounter = Counter()
+bio = BytesIO()
+out_file_id = 1
+
+def get_next_out_file_path(parent_dir: Path, file_path: Path):
+    if out_file_id == 1:
+        if args.all_directory_mode:
+            next_out_file_path = parent_dir / "jsonl_reworked" / (args.all_directory_mode + '.jsonl')
+        else:
+            next_out_file_path = parent_dir / "jsonl_reworked" / file_path.name
+    else:
+        if args.all_directory_mode:
+            next_out_file_path = parent_dir / "jsonl_reworked" / f"{args.all_directory_mode}-{out_file_id}.jsonl"
+        else:
+            filename_without_ext, file_ext_name = file_path.name.rsplit('.', 1)
+            next_out_file_path = parent_dir / "jsonl_reworked" / f"{filename_without_ext}-{out_file_id}.{file_ext_name}"
+    return next_out_file_path
+
+def out_file(file_path: Path):
+    out_file_dir = file_path.parent / "jsonl_reworked"
+    global is_first
+    if is_first:
+        if os.path.exists(out_file_dir):
+            print(f"请确保{out_file_dir}目录为空，否则其内容可能会被覆盖。如不希望请直接结束本程序。")
+            if input("请输入Y以确认继续进行:") != 'Y':
+                print("程序退出...")
+                exit(0)
+        else:
+            os.makedirs(out_file_dir)
+        is_first = False
+    del out_file_dir
+    global out_file_id
+    for lineidx, linejson in enumerate(gen_new_style_line(file_path, True)):
+        if (str(file_path), lineidx) not in valid_line_idx_set:
             continue
         linejsonfilename = linejson['文件名']
         filename2linecounter[linejsonfilename] += 1
         dedup_bytes = linejson["zh_text"].encode("utf-8")
         zhmd5 = hashlib.md5(dedup_bytes)
-        digest = zhmd5.digest() + len(dedup_bytes).to_bytes(4)
+        digest = zhmd5.digest() + (len(dedup_bytes) % 256).to_bytes(1, signed=False)
         zh_text_set = filename2zh_text_digest[linejsonfilename]
         _prvlen = len(zh_text_set)
         zh_text_set.discard(digest)
@@ -382,6 +407,7 @@ def process_file(file_path: Path):
         linejson['zh_text_md5'] = zhmd5.hexdigest()
         outjsonbytes = (json.dumps(linejson, ensure_ascii=False, sort_keys=True) + '\n').encode('utf-8') # 这个是LF格式的换行
         if bio.tell() + len(outjsonbytes) > args.bytes_limit:
+            next_out_file_path = get_next_out_file_path(file_path.parent, file_path)
             with open(next_out_file_path, "wb") as fo:
                 print("out file:",next_out_file_path)
                 fo.write(bio.getbuffer().tobytes())
@@ -390,25 +416,77 @@ def process_file(file_path: Path):
                 bio.seek(0)
                 bio.truncate()
                 out_file_id += 1
-                next_out_file_path = file_path.parent / "jsonl_reworked" / f"{filename_without_ext}-{out_file_id}.{file_ext_name}"
         bio.write(outjsonbytes)
-    if bio.tell() > 0:
-        print("out file:",next_out_file_path)
-        with open(next_out_file_path, "wb") as fo:
-            fo.write(bio.getbuffer().tobytes())
-            # bio.seek(0)
-            # bio.readinto(fo)
 
-if args.directory:
-    for filename in os.listdir(args.directory):
-        if filename.endswith('.jsonl'):
-            print('[directory] filename:',filename)
-            process_file(Path(os.path.join(args.directory, filename)))
-elif args.input:
-    print('[single file] filename:',args.input)
-    process_file(Path(args.input))
-else:
-    print("请提供一个目录或输入文件路径。")
-    exit(0)
+if __name__ == "__main__":
+    if args.directory:
+        if not args.all_directory_mode:
+            for filename in os.listdir(args.directory):
+                if filename.endswith('.jsonl'):
+                    print('[directory] filename:',filename)
+                    file_path = Path(os.path.join(args.directory, filename))
+                    process_file(file_path)
+                    out_file(file_path)
+                    first_warn_unk_key.clear()
+                    first_warn_other_texts_key_check.clear()
+                    filename2zh_text_digest.clear()
+                    filename2low_quality_count.clear()
+                    filename2linecount.clear()
+                    valid_line_idx_set.clear()
+                    filename2zh_text_dedup_count.clear()
+                    filename2linedigest.clear()
+                    filename2linecounter.clear()
+                    if bio.tell() > 0:
+                        next_out_file_path = get_next_out_file_path(file_path.parent, file_path)
+                        print("out file:",next_out_file_path)
+                        with open(next_out_file_path, "wb") as fo:
+                            fo.write(bio.getbuffer().tobytes())
+                    bio.seek(0)
+                    bio.truncate()
+                    out_file_id = 1
+        else:
+            cachepath = Path(os.path.join(args.directory, "stat.pkl"))
+            if cachepath.exists():
+                with open(cachepath, "rb") as f:
+                    print("Load cache file:",cachepath)
+                    first_warn_unk_key,first_warn_other_texts_key_check,filename2zh_text_digest,filename2low_quality_count,filename2linecount,valid_line_idx_set,filename2zh_text_dedup_count,filename2linedigest = pickle.load(f)
+            else:
+                for filename in os.listdir(args.directory):
+                    if filename.endswith('.jsonl'):
+                        print('[reading directory] filename:',filename)
+                        process_file(Path(os.path.join(args.directory, filename)))
+                with open(cachepath, "wb") as f:
+                    pickle.dump(
+                        (
+                            first_warn_unk_key,
+                            first_warn_other_texts_key_check,
+                            filename2zh_text_digest,
+                            filename2low_quality_count,
+                            filename2linecount,
+                            valid_line_idx_set,
+                            filename2zh_text_dedup_count,
+                            filename2linedigest,
+                        ), f, pickle.HIGHEST_PROTOCOL
+                    )
+                    print("Write cache file:",cachepath)
+            for filename in os.listdir(args.directory):
+                if filename.endswith('.jsonl'):
+                    print('[output] filename:',filename)
+                    out_file(Path(os.path.join(args.directory, filename)))
+            if bio.tell() > 0:
+                next_out_file_path = get_next_out_file_path(Path(args.directory), None)
+                print("out file:",next_out_file_path)
+                with open(next_out_file_path, "wb") as fo:
+                    fo.write(bio.getbuffer().tobytes())
+            bio.seek(0)
+            bio.truncate()
 
-input("处理完毕，回车关闭")
+    elif args.input:
+        print('[single file] filename:',args.input)
+        process_file(Path(args.input))
+        out_file(Path(args.input))
+    else:
+        print("请提供一个目录或输入文件路径。")
+        exit(0)
+
+    # input("处理完毕，回车关闭")
